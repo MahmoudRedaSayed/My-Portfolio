d59fa396912ad9834033be7a84d27e92a570d283
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2023-02-20 12:09:29 +0200

    edit projects section

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col,Button } from "react-bootstrap";
+import { Container, Row, Col ,Button} from "react-bootstrap";
 import { BsGithub } from "react-icons/bs";
 
 import ProjectCard from "./ProjectCards";
